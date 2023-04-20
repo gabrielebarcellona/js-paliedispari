593a8c93546f5aca23326d17568fca3ff06e9fa9
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-20 16:50:57 +0200

    add structure

---
+++ esercizio1/script.js
@@ -0,0 +1,13 @@
+/*Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+.*/
+
+
+eleParol = prompt('inserisci una parola')
+
+function parola ;
+
+if(parola){
+    
+}
